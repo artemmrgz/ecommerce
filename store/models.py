06cd1d67cfadb293d12f17ebf39ba4e2916fe47b
Artem Marhaza <artemmrgz@gmail.com>
2022-03-14 16:41:27 +0100

    update models

--- store/models.py
+++ store/models.py
@@ -15,9 +15,18 @@ class Product(models.Model):
     name = models.CharField(max_length=200)
     price = models.FloatField()
     digital = models.BooleanField(default=False, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
+
+    @property
+    def imageURL(self):
+        try:
+            url = self.image.url
+        except:
+            url = ''
+        return url
 
 
 class Order(models.Model):
@@ -29,12 +38,28 @@ class Order(models.Model):
     def __str__(self):
         return str(self.id)
 
+    @property
+    def cart_total(self):
+        order_items = self.orderitem_set.all()
+        total = sum([item.total_price for item in order_items])
+        return total
+
+    @property
+    def cart_items(self):
+        order_items = self.orderitem_set.all()
+        total = sum([item.quantity for item in order_items])
+        return total
+
 
 class OrderItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
     quantity = models.IntegerField(default=0, null=True, blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def total_price(self):
+        return self.quantity * self.product.price
 
 
 class ShippingAddress(models.Model):
